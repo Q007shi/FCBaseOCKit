bab48d12081f162137db41b66b0b60285b6479b3
Q007shi <2585299617@qq.com>
2021-03-03 12:49:15 +0800

    1

---
+++ Example/Pods/Target Support Files/FCBaseOCKit/FCBaseOCKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FCBaseOCKit : NSObject
+@end
+@implementation PodsDummy_FCBaseOCKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-FCBaseOCKit_Tests/Pods-FCBaseOCKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FCBaseOCKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_FCBaseOCKit_Tests
+@end
